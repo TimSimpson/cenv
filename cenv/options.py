426b8803de3cbfb113ffffb8068683cc7585a8b5
Tim Simpson <timsimpson4@gmail.com>
2017-11-18 13:02:39 -0600

    Adds quicker way of running tox

--- cenv/options.py
+++ cenv/options.py
@@ -1,7 +1,21 @@
-from typing import Optional  # NOQA
-from . import types as t  # NOQA
+import os
+
+import typing as t  # NOQA
+from . import types as ct  # NOQA
 
 
 class Options(object):
     def __init__(self):
-        self._root_directory = None  # type: Optional[t.FilePath]
+        self._root_directory = None  # type: t.Optional[ct.FilePath]
+
+    def _from_root(self, path):
+        return os.path.join(self._root_directory, path)
+
+    @property
+    def environments(self):
+        # type: () -> ct.FilePath
+        return self._from_root('envs')
+
+    @property
+    def toolchains(self):
+        return self._from_root('toolchains')
