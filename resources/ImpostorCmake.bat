167781e5b8e7c96578cbd196be0f32d4cf044a25
Tim Simpson <timsimpson4@gmail.com>
2017-11-27 21:54:20 -0600

    Fixes cmake Windows wrapper no args bug

--- resources/ImpostorCmake.bat
+++ resources/ImpostorCmake.bat
@@ -2,21 +2,24 @@
 
 
 set args=%*
-REM replace " with Q since quotes mess things up
-set args=%args:"=Q%
-REM remove "--build" from the args
+if [%args%] == [] goto normal
+if "%CGET_PREFIX%"=="" goto normal
+
+REM replace " with 'ESCAPE_QUOTE_HACK' since quotes mess things below up
+set args=%args:"=ESCAPE_QUOTE_HACK%
+
+REM remove "--build" from the args to from the variable `args_minus_build`
 set args_minus_build=%args:--build=%
-if "%CGET_PREFIX%"=="" ( GOTO normal ) ELSE (
-    REM if --build isn't in the text, don't inject.
-    REM Yes, this is a horrible hack.
-    IF "%args%"=="%args_minus_build%" (
-        GOTO inject
-    )
+
+REM if `--build` isn't in the text, don't inject.
+REM Yes, I know this is a horrible hack and I feel terrible about it.
+IF "%args%"=="%args_minus_build%" (
+    GOTO inject
+) else (
     GOTO normal
 )
 
 :normal
-    echo CGET_PREFIX not set
     cmake %*
 GOTO the_end
 
