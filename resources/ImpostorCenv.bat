4932fde57a04a53bf94e11baced4075f9e779b7a
Tim Simpson <timsimpson4@gmail.com>
2017-11-27 20:01:03 -0600

    Fix issue where `cenv list` could active envs

--- resources/ImpostorCenv.bat
+++ resources/ImpostorCenv.bat
@@ -8,10 +8,16 @@ if "%CENV_BIN_ROOT%"=="" (
     set CENV_BIN_ROOT=%~dp0
 )
 
+if exist "%CENV_ROOT%\set-vars.bat" (
+    del "%CENV_ROOT%\set-vars.bat"
+)
+
 %CENV_BIN_ROOT%\cenv.exe %*
 if errorlevel 1 (
     exit /b %errorlevel%
 ) else (
-    CALL "%CENV_ROOT%\set-vars.bat"
-    exit /b %errorlevel%
+    if exist "%CENV_ROOT%\set-vars.bat" (
+        CALL "%CENV_ROOT%\set-vars.bat"
+        exit /b %errorlevel%
+    )
 )
