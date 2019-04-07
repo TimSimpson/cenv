9d3870bb7595a4764b54450ab71a43789a45d94e
Tim Simpson <timsimpson4@gmail.com>
2019-04-07 11:39:42 -0500

    Revert "make things less crazy"
    This reverts commit b50fd032958b9079f82e77f544fa50194af64da6.

--- tox/run_py36.py
+++ tox/run_py36.py
@@ -53,3 +53,63 @@ def run_all() -> int:
 print(sys.executable)
 
 sys.exit(run_all())
+=======
+from concurrent import futures
+import pathlib
+import subprocess
+import sys
+
+with open(pathlib.Path(__file__).parent / "py36_path.txt") as f:
+    PYTHON_BIN = f.read()
+
+
+CENV_DIR = pathlib.Path(__file__).parent / ".." / "cenv"
+BIN_DIR = pathlib.Path(PYTHON_BIN).parent
+MYPY_OPTIONS = [
+    "--strict-optional",
+    "--ignore-missing-imports",
+    "--disallow-untyped-calls",
+    "--disallow-untyped-defs",
+    str(CENV_DIR),
+]
+
+print(CENV_DIR)
+
+def flake8() -> int:
+    return subprocess.call(f"{BIN_DIR / 'flake8'} {CENV_DIR}", shell=True)
+
+
+def mypy3() -> int:
+    print(f"{BIN_DIR / 'mypy'} " + " ".join(MYPY_OPTIONS))
+    return subprocess.call(f"{BIN_DIR / 'mypy'} " + " ".join(MYPY_OPTIONS), shell=True)
+
+
+def mypy2() -> int:
+    return subprocess.call(f"{BIN_DIR / 'mypy'} --py2 " + " ".join(MYPY_OPTIONS), shell=True)
+
+
+def coverage() -> int:
+    return (
+        subprocess.call(f"{BIN_DIR / 'coverage'} run -m py.test {CENV_DIR}/tests", shell=True)
+        or subprocess.call(f"{BIN_DIR / 'coverage'} report -m", shell=True)
+        or subprocess.call(f"{BIN_DIR / 'coverage'} html --directory coverage/py36", shell=True)
+    )
+
+
+def run_all() -> int:
+    executor = futures.ThreadPoolExecutor(max_workers=4)
+    async_results = []
+    async_results.append(executor.submit(flake8))
+    async_results.append(executor.submit(mypy3))
+    async_results.append(executor.submit(mypy2))
+    async_results.append(executor.submit(coverage))
+
+    for f in futures.as_completed(async_results):
+        if f.result() != 0:
+            return f.result()
+
+    return 0
+
+print(sys.executable)
+
+sys.exit(run_all())
